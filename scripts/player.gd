dcd7f097a6410a0d0443a5f6798e1c9f0c61ad1f
pablo-Acha <pabloate901@gmail.com>
2025-09-10 18:24:53 -0400

    animation tree

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,40 +1,3 @@
-#extends CharacterBody2D
-#
-#@export var move_speed: float
-#@onready var animated_sprite = $Sprite2D
-#var is_facing_right = true
-#
-#func _physics_process(delta):
-	#move()
-	#flip()
-	#update_animations()
-	#move_and_slide()
-	#
-	#
-#func update_animations():
-	#if velocity.x or velocity.y:
-		#animated_sprite.play("run")
-	#else:
-		#animated_sprite.play("idle")
-	#
-#func move():
-	#var input_axis = Vector2(
-		#Input.get_axis("move_left", "move_right"),
-		#Input.get_axis("move_up", "move_down")
-	#)
-	#
-	## Normaliza para que en diagonales no sea más rápido
-	#if input_axis != Vector2.ZERO:
-		#input_axis = input_axis.normalized()
-	#
-	#velocity = input_axis * move_speed
-#
-#func flip():
-	#if (is_facing_right and velocity.x < 0) or (not is_facing_right and velocity.x > 0):
-		#scale.x *= -1
-		#is_facing_right = not is_facing_right
-
-
 extends CharacterBody2D
 
 @export var move_speed: float
@@ -47,7 +10,6 @@ func _ready():
 
 func _physics_process(delta):
 	move()
-	#flip()
 	update_animations()
 	move_and_slide()
 
@@ -65,9 +27,7 @@ func update_animations():
 	elif velocity.x<0:
 		is_facing_right = false
 	var dir = 1 if is_facing_right else -1
-	
 	anim_tree.set("parameters/Idle/blend_position", dir)
-		
 	anim_tree.set("parameters/Run/blend_position", dir)
 
 func move():
@@ -81,8 +41,6 @@ func move():
 
 	velocity = input_axis * move_speed
 
-#func flip():
-	## Solo flip horizontal
-	#if (is_facing_right and velocity.x < 0) or (not is_facing_right and velocity.x > 0):
-		#scale.x *= -1
-		#is_facing_right = not is_facing_right
+#le rebaja vida
+func _on_hurt_box_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	pass # Replace with function body.
